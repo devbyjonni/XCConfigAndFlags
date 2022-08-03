fd9c9901880377b01cd9b5edc823a207f30a345f
Jonni Åkesson <enterthesource@me.com>
2022-08-03 20:23:59 +0200

    Initial Commit

---
+++ XCConfigAndFlags/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCConfigAndFlags
+//
+//  Created by Jonni Akesson on 2022-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCConfigAndFlags/XCConfigAndFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCConfigAndFlagsApp.swift
+//  XCConfigAndFlags
+//
+//  Created by Jonni Akesson on 2022-08-03.
+//
+
+import SwiftUI
+
+@main
+struct XCConfigAndFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
